b157c88fccc6429ba4f5cebdd86f7a64e2ad1d5d
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-03-26 13:50:14 +0100

    format the unformatted sql

--- etl/src/sql/migration/01trajectories.sql
+++ etl/src/sql/migration/01trajectories.sql
@@ -1,4 +1,27 @@
 CREATE SEQUENCE traj_seq;
-CREATE TABLE trajectories (id INTEGER PRIMARY KEY DEFAULT nextval('traj_seq'), mmsi UINTEGER, time_begin TIMESTAMP, duration INTERVAL);
-CREATE OR REPLACE VIEW latest_trajectories AS (SELECT DISTINCT ON (mmsi) * FROM trajectories ORDER BY time_begin DESC);
-CREATE OR REPLACE VIEW oldest_message AS (SELECT DISTINCT ON (mmsi) mmsi, timestamp as time_begin FROM ais_point ORDER BY timestamp);
+
+CREATE TABLE trajectories (
+    id INTEGER PRIMARY KEY DEFAULT nextval('traj_seq'),
+    mmsi UINTEGER,
+    time_begin TIMESTAMP,
+    duration INTERVAL
+);
+
+CREATE OR REPLACE VIEW latest_trajectories AS (
+    SELECT DISTINCT
+        ON (mmsi) *
+    FROM
+        trajectories
+    ORDER BY
+        time_begin DESC
+);
+
+CREATE OR REPLACE VIEW oldest_message AS (
+    SELECT DISTINCT
+        ON (mmsi) mmsi,
+        timestamp AS time_begin
+    FROM
+        ais_point
+    ORDER BY
+        timestamp
+);
