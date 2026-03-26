81e20e6a5850b92d61db6f0327dbd1bad345b6cb
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-03-26 13:50:49 +0100

    added wal logs to gitignore

---
+++ etl/src/sql/migration/02confidence_interval.sql
@@ -0,0 +1,17 @@
+Begin;
+
+CREATE OR REPLACE VIEW main.confidence_by_mmsi AS (
+    SELECT
+        mmsi,
+        min(draught) AS mi,
+        max(draught) AS ma,
+        quantile_disc(draught, [0.05, 0.95]) AS confidence -- not using `quantile_cont` because it will interpolate measurements
+    FROM
+        main.ais_data
+    GROUP BY
+        mmsi
+    ORDER BY
+        ma - mi DESC
+);
+
+COMMIT;
