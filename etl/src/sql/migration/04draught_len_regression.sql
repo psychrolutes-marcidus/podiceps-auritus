fe675e9a17b8125965fc0d41d422f8d3ebd823b0
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-03-27 13:17:25 +0100

    just playing

---
+++ etl/src/sql/migration/04draught_len_regression.sql
@@ -0,0 +1,82 @@
+WITH
+    av AS (
+        SELECT
+            avg(draught::decimal(10, 1)) AS ad,
+            avg(ship_length::decimal(10, 1)) AS al
+        FROM
+            main.ais_data
+    ),
+    base AS (
+        SELECT
+            draught::decimal(10, 1) AS draught_y,
+            draught::decimal(10, 1) - av.ad AS draught_y_centered,
+            ship_length::decimal(10, 1) AS ship_length_x1,
+            ship_length::decimal(10, 1) - av.al AS ship_length_x1_centered
+        FROM
+            main.ais_data,
+            av
+        WHERE
+            ship_length IS NOT NULL
+            AND draught IS NOT NULL
+    ),
+    regress AS (
+        SELECT
+            AVG(draught_y) - avg(ship_length_x1) * SUM(draught_y_centered * ship_length_x1_centered) / sum(ship_length_x1_centered * ship_length_x1_centered) AS const_coef,
+            SUM(draught_y_centered * ship_length_x1_centered) / SUM(ship_length_x1_centered * ship_length_x1_centered) AS x1_coef
+        FROM
+            base
+    )
+SELECT
+    *
+FROM
+    regress;
+
+--\nyt forsøg
+-- csv
+COPY (
+    SELECT
+        draught,
+        ship_length
+    FROM
+        main.ais_data
+    WHERE
+        draught IS NOT NULL
+        AND ship_length IS NOT NULL
+) TO 'draught_len.csv';
+
+--\csv
+WITH
+    base AS (
+        SELECT
+            draught::decimal(10, 1) AS draught_y,
+            draught::decimal(10, 1) - AVG(draught::decimal(10, 1)) over () AS draught_y_centered,
+            ship_length::decimal(10, 1) AS ship_length_x1,
+            ship_length::decimal(10, 1) - AVG(ship_length::decimal(10, 1)) AS ship_length_x1_centered
+        FROM
+            main.ais_data
+        WHERE
+            ship_length IS NOT NULL
+            AND draught IS NOT NULL
+    ),
+    regress AS (
+        SELECT
+            AVG(draught_y) - avg(ship_lenght_x1) * SUM(draught_y_centered * ship_length_x1_centered) / sum(ship_length_x1_centered * ship_length_x1_centered) AS const_coef,
+            SUM(draught_y_centered * ship_length_x1_centered) / SUM(ship_length_x1_centered, ship_length_x1_centered) AS x1_coef
+        FROM
+            base
+    )
+SELECT
+    *
+FROM
+    regress;
+
+SELECT
+    draught::decimal(10, 1) AS draught_y,
+    draught::decimal(10, 1) - AVG(draught::decimal(10, 1)) over () AS draught_y_centered,
+    ship_length::decimal(10, 1) AS ship_length_x1,
+    ship_length::decimal(10, 1) - AVG(ship_length::decimal(10, 1)) AS ship_length_x1_centered
+FROM
+    main.ais_data
+WHERE
+    ship_length IS NOT NULL
+    AND draught IS NOT NULL;
