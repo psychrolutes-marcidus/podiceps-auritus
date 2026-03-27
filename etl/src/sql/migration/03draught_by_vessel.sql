612745fb52f219e9b2d01f710b883141e2907684
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-03-27 10:49:32 +0100

    bump

---
+++ etl/src/sql/migration/03draught_by_vessel.sql
@@ -0,0 +1,10 @@
+-- PIVOT ais_data ON ship_type USING avg(draught);
+CREATE OR REPLACE VIEW vessel_attributes.draught_nulls_by_ship_type AS (
+    SELECT
+        ship_type,
+        (COUNT(draught) / count())::double AS draughts_null, -- count(draught) counts non nulls, count() counts total including nulls
+    FROM
+        ais_data
+    GROUP BY
+        ship_type
+);
