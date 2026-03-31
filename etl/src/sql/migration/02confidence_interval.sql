289e983566bf43772ad03f2a5a478014f713686d
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-03-31 11:56:19 +0200

    fix the fix

--- etl/src/sql/migration/02confidence_interval.sql
+++ etl/src/sql/migration/02confidence_interval.sql
@@ -1,13 +1,14 @@
 Begin;
 
-
 -- might be bad if the underlying vessel changes, but MMSI does not.
 CREATE OR REPLACE VIEW main.confidence_by_mmsi AS (
     SELECT
         mmsi,
         min(draught) AS mi,
         max(draught) AS ma,
-        quantile_disc(draught, [0.05, 0.95]) AS confidence -- not using `quantile_cont` because it will interpolate measurements
+        quantile_disc(draught, [0.05, 0.95]) AS confidence, -- not using `quantile_cont` because it will interpolate measurements
+        count(draught) AS num_draughts,
+        count(DISTINCT draught) AS distinct_draughts
     FROM
         main.ais_data
     GROUP BY
