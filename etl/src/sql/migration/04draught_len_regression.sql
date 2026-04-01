4158cb7c8f20c53b0a13dfb157e10fc88a5fca9a
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-04-01 13:57:00 +0200

    not so interesting after all, seems to be very slow when run on 1 years worth of data

--- etl/src/sql/migration/04draught_len_regression.sql
+++ etl/src/sql/migration/04draught_len_regression.sql
@@ -1,5 +1,3 @@
-BEGIN;
-
 CREATE SCHEMA IF NOT EXISTS vessel_stats;
 
 CREATE OR REPLACE VIEW vessel_stats.linear_regression AS (
@@ -18,4 +16,56 @@ CREATE OR REPLACE VIEW vessel_stats.linear_regression AS (
         ship_type
 );
 
-COMMIT;
+SELECT
+    -- vt.mmsi,
+    ship_type,
+    REGR_SLOPE(draught, ship_length) AS slope, -- growth in draught as a function of ship length
+    REGR_INTERCEPT(draught, ship_length) AS intercept, -- draught-offset at ship_length=0
+    REGR_R2(draught, ship_length) AS r_squared,
+    count(*) AS messages_count
+FROM
+    main.ais_data
+WHERE
+    lat != 91 -- REGR_{SLOPE | INTERCEPT | R2} ignore null values
+GROUP BY
+    ship_type
+ORDER BY
+    ship_type;
+
+/* SELECT
+*
+FROM
+ais_data ad
+JOIN main.confidence_by_vessel cbv ON ad.ship_type = cbv.ship_type
+AND ad.draught >= cbv.confidence[1]
+AND ad.draught <= cbv.confidence[2]; */
+
+-- regression 
+WITH
+    draughts_within AS (
+        SELECT
+            cbv.ship_type,
+            ad.ship_length,
+            ad.draught,
+            ad.lat
+        FROM
+            main.ais_data ad
+            JOIN main.confidence_by_vessel cbv ON ad.ship_type = cbv.ship_type
+            AND ad.draught >= cbv.confidence[1]
+            AND ad.draught <= cbv.confidence[2]
+    )
+SELECT
+    -- vt.mmsi,
+    ship_type,
+    REGR_SLOPE(draught::decimal(10, 1), ship_length) AS slope, -- growth in draught as a function of ship length
+    REGR_INTERCEPT(draught, ship_length) AS intercept, -- draught-offset at ship_length=0
+    REGR_R2(draught, ship_length) AS r_squared,
+    count(*) AS messages_count
+FROM
+    draughts_within
+WHERE
+    lat != 91 -- REGR_{SLOPE | INTERCEPT | R2} ignore null values
+GROUP BY
+    ship_type
+ORDER BY
+    ship_type;
