b59a097f7412c7fbe570e719e0a3c6965d8a0bfa
Anders Rask Sørensen <61500734+Uewotm90@users.noreply.github.com>
2026-04-03 10:47:25 +0200

    moar scripts (#45)
    * remove order by clause since it serves no purpose besides testing
    
    * moar scripts
    
    * Apply suggestion from @razwaz
    
    fix the fix
    
    Co-authored-by: razwaz <93257877+razwaz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: razwaz <93257877+razwaz@users.noreply.github.com>

---
+++ etl/src/sql/migration/05confidence_by_vessel_type.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW main.confidence_by_vessel AS (
+    SELECT
+        ship_type,
+        min(draught) AS mi,
+        max(draught) AS ma,
+        QUANTILE_DISC(draught, [0.05, 0.95]) AS confidence,
+        count(draught) AS num_draughts,
+        count(DISTINCT draught) AS distinct_draughts
+    FROM
+        main.ais_data
+    GROUP BY
+        ship_type
+);
