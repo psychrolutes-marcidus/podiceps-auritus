3d432f8d13d99a9287c8b021e6d8cf0a706a60d6
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2026-04-07 09:27:00 +0200

    histogram is gramming

--- etl/src/sql/migration/08histogram.sql
+++ etl/src/sql/migration/08histogram.sql
@@ -1,10 +1,9 @@
-SELECT
-    ship_type,
-    unnest(
-        map_entries(histogram(draught)),
-        recursive := TRUE
-    )
-FROM
-    ais_data
-GROUP BY
-    ship_type;
+CREATE OR REPLACE TABLE vessel_stats.draught_histogram_by_ship_types AS (
+    SELECT
+        ship_type,
+        histogram(draught) AS "histogram" -- key: draught, value: #measurements
+    FROM
+        ais_data
+    GROUP BY
+        ship_type
+);
