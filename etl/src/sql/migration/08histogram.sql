b30416b3e5972def02839a29fe6184a7ad03c79b
Anders Rask Sørensen <61500734+Uewotm90@users.noreply.github.com>
2026-04-15 09:52:20 +0200

    Histogram (#57)
    * histogram
    
    * histogram is gramming
    
    * Update etl/src/sql/migration/08histogram.sql
    
    Co-authored-by: Rasmus Hald Nielsen <37369606+Murmeldyret@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Rasmus Hald Nielsen <37369606+Murmeldyret@users.noreply.github.com>

---
+++ etl/src/sql/migration/08histogram.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW vessel_stats.draught_histogram_by_ship_types AS (
+    SELECT
+        ship_type,
+        histogram(draught) AS "histogram" -- key: draught, value: #measurements
+    FROM
+        ais_data
+    GROUP BY
+        ship_type
+);
